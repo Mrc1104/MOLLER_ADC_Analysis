904f383d104e3494bef24ea3d31c98ec845eb30d
Ryan Conaway <mc321015@ohio.edu>
2025-11-11 11:35:26 -0500

    Generalizes Macro.cpp to multiple rootfiles

--- Linearity/main/Macro.cpp
+++ Linearity/main/Macro.cpp
@@ -21,6 +21,8 @@
 #include <utility>
 #include "TRootCanvas.h"
 #include "TGraphErrors.h"
+
+#define OFFSET 20
 enum SOFTWARE_CHANNEL
 {
 	CHAN_0 = 0,
@@ -40,12 +42,12 @@ double GetMean(const std::vector<double> &v)
 }
 double GetRMS(const std::vector<double> &v, const double mean)
 {
-	double second_moment = 0.0;
+	double sum = 0.0;
 	for( auto d : v ) {
-		second_moment = d * d;
+		sum += (d - mean)*(d - mean);
 	}
-	second_moment /= v.size();
-	return TMath::Sqrt( second_moment - mean*mean );
+	sum /= v.size();
+	return TMath::Sqrt( sum );
 }
 
 Signal_tStmp Find_Valid_Signal_Range(ROOT::RNTupleReader* Reader, SOFTWARE_CHANNEL chan)
@@ -108,19 +110,49 @@ Signal_tStmp Find_Valid_Signal_Range(ROOT::RNTupleReader* Reader, SOFTWARE_CHANN
 	}
 	std::cout << "Max Found to be " << max << " at tStmp: " << MaxTimeStamp << "\n";
 	std::cout << "Min Found to be " << min << " at tStmp: " << MinTimeStamp << "\n";
-	return Signal_tStmp{MinTimeStamp, MaxTimeStamp};
+	return (MinTimeStamp < MaxTimeStamp) ? Signal_tStmp{MinTimeStamp, MaxTimeStamp} : Signal_tStmp{MaxTimeStamp, MinTimeStamp};
+}
+
+void divide_canvas_algorithm(TCanvas &c, const int size)
+{
+	[[maybe_unused]]
+	int row = 1, col = 1, col_counter = 0;
+	while(row*col < size) {
+		col++;
+		if(row < col) 
+		{
+			row++;
+			col--;
+		}
+
+	}
+	c.Divide(row,col);
 }
 
 
 int main(int argc, char** argv)
 {
-	auto cResidualMeans      = std::make_unique<TCanvas>("ResidualMeans");
+	
+	// Open File
+	std::vector<std::string> vFiles = {
+		"../Rootfiles/moller_stream_molleradcse05_110.root",
+		"../Rootfiles/moller_stream_molleradcse05_111.root",
+		"../Rootfiles/moller_stream_molleradcse05_112.root",
+		"../Rootfiles/moller_stream_molleradcse05_113.root",
+		"../Rootfiles/moller_stream_molleradcse05_116.root",
+		"../Rootfiles/moller_stream_molleradcse05_117.root",
+		"../Rootfiles/moller_stream_molleradcse05_118.root",
+		"../Rootfiles/moller_stream_molleradcse05_119.root",
+	};
+
+	const size_t N_ADC_CHAN = 2*vFiles.size();
+	auto cResidualMeans = std::make_unique<TCanvas>("ResidualMeans");
 	auto gResidualMeans = std::make_unique<TGraphErrors>();
 
 	auto cResidual = std::make_unique<TCanvas>("Residual");
-	cResidual->Divide(2);
-	std::unique_ptr<TGraph> gResidual[2];
-	for(int i = 0; i < 2; i++) {
+	divide_canvas_algorithm(*cResidual, N_ADC_CHAN);
+	std::unique_ptr<TGraph> gResidual[N_ADC_CHAN];
+	for(size_t i = 0; i < N_ADC_CHAN; i++) {
 		gResidual[i] = std::make_unique<TGraph>();
 	}
 
@@ -132,91 +164,94 @@ int main(int argc, char** argv)
 
 
 	auto cRange =  std::make_unique<TCanvas>("cRange");
-	cRange->Divide(2);
-	std::unique_ptr<TGraph> gRange[2];
-	for(int i = 0; i < 2; i++) {
+	divide_canvas_algorithm(*cRange, N_ADC_CHAN);
+	std::unique_ptr<TGraph> gRange[N_ADC_CHAN];
+	for(int i = 0; i < N_ADC_CHAN; i++) {
   		gRange[i] = std::make_unique<TGraph>();
 	}
 
 
 	auto fsave = std::make_unique<TFile>("LinearityStats.root", "RECREATE");
 
-	for(int i = 0; i < 2; i++) {
-		SOFTWARE_CHANNEL chan = (SOFTWARE_CHANNEL)i;
-
-		// Open File
-		std::string file_name = "../Rootfiles/moller_stream_molleradcse05_110.root";
+	int adc_channel = -2;
+	for( auto const &file_name : vFiles )
+	{
 		std::unique_ptr<ROOT::RNTupleReader> Reader = ROOT::RNTupleReader::Open("DataTree",file_name.c_str());
-		const Signal_tStmp extrema = Find_Valid_Signal_Range(Reader.get(), CHAN_0);
+		adc_channel += 3;
+		for(int i = 0; i < 2; i++) {
+			SOFTWARE_CHANNEL chan = (SOFTWARE_CHANNEL)i;
+			adc_channel -= chan;
+			std::cout << "adc_channel: " << adc_channel << std::endl;
 
-		// Draw +-10% to check
-		cRange->cd(chan+1);
-		std::vector<double> voltage;
-		std::vector<double> timestamps;
-		ROOT::RNTupleView<tDataSamples> data = Reader->GetView<tDataSamples>("SampleStream");
-		for( auto entry : Reader->GetEntryRange() ) {
-			const auto& ch_data  = (chan == CHAN_1) ? data(entry).ch1_data : data(entry).ch0_data;
-			const auto& tStmp    = data(entry).tStmp;
-			if(tStmp[tStmp.size()-1] < extrema.min) continue;
-			for(size_t index = 0; index < ch_data.size(); index++) {
-				if(tStmp[index] >= extrema.min-10 && tStmp[index] <= extrema.max+10) {
-					gRange[chan]->AddPoint(tStmp[index], ch_data[index]);
-					if(tStmp[index] >= extrema.min && tStmp[index] <= extrema.max) {
-						voltage.push_back( ch_data[index] );
-						timestamps.push_back( tStmp[index] );
+			const Signal_tStmp extrema = Find_Valid_Signal_Range(Reader.get(), chan);
+
+			// Draw +-10% to check
+			cRange->cd(adc_channel+1);
+			std::vector<double> voltage;
+			std::vector<double> timestamps;
+			ROOT::RNTupleView<tDataSamples> data = Reader->GetView<tDataSamples>("SampleStream");
+			for( auto entry : Reader->GetEntryRange() ) {
+				const auto& ch_data  = (chan == CHAN_1) ? data(entry).ch1_data : data(entry).ch0_data;
+				const auto& tStmp    = data(entry).tStmp;
+				if(tStmp[tStmp.size()-1] < extrema.min) continue;
+				for(size_t index = 0; index < ch_data.size(); index++) {
+					if(tStmp[index] >= extrema.min-10 && tStmp[index] <= extrema.max+10) {
+						gRange[adc_channel]->AddPoint(tStmp[index], ch_data[index]);
+						if(tStmp[index] >= extrema.min+OFFSET && tStmp[index] <= extrema.max-OFFSET) {
+							voltage.push_back( ch_data[index] );
+							timestamps.push_back( tStmp[index] );
+							// std::cout << ch_data[index] << "\t" << tStmp[index] << std::endl;
+						}
 					}
 				}
 			}
+			gRange[adc_channel]->SetTitle(Form("Soft. Chan %d vs Time; tStmp [ms]; ch%d_data",chan, chan));
+			gRange[adc_channel]->Draw("AP");
+
+			// Fit with the exact range we care about
+			TF1* fit = new TF1(Form("fit_adc_chan%d", adc_channel), "pol1", extrema.min+OFFSET, extrema.max-OFFSET);
+			gRange[adc_channel]->Fit(fit, "R");
+
+			// Draw slope vs Chan
+			cSlope->cd();
+			gSlope->AddPointError(adc_channel, fit->GetParameter(1), 0, fit->GetParError(1));
+			gSlope->SetTitle("Slope Vs ADC Chan; ADC Chan; Slope");
+			gSlope->SetMarkerStyle(8);
+			gSlope->Draw("AP");
+
+			// Draw Intercept vs Chan
+			cIntercept->cd();
+			gIntercept->AddPointError(adc_channel, fit->GetParameter(0), 0, fit->GetParError(0));
+			gIntercept->SetTitle("Intercept Vs ADC Chan; ADC Chan; Intercept");
+			gIntercept->SetMarkerStyle(8);
+			gIntercept->Draw("AP");
+
+
+			// Compute Residual
+			cResidual->cd(adc_channel+1);
+			std::vector<double> residual;
+			for( size_t index = 0; index < voltage.size(); index++ ) {
+				double time   = timestamps[index];
+				double actual = voltage[index];
+				double eval   = fit->Eval(time);
+				auto res = residual.insert(std::end(residual), eval - actual);
+				// std::cout << residual_counter << "\t" << residual << "\n";
+				gResidual[adc_channel]->AddPoint(time,*res);
+			}
+			double avg_residual = GetMean(residual);
+			double rms_residual = GetRMS(residual, avg_residual);
+
+			gResidual[adc_channel]->SetTitle(Form("Residual Vs ADC Chan%d; ADC Chan %d; Residual", adc_channel,adc_channel));
+			gResidual[adc_channel]->Draw("AP");	
+			std::cout << "Avg Residual: " << avg_residual << "\n";
+			std::cout << "RMS: " << rms_residual << "\n";
+
+			cResidualMeans->cd();
+			gResidualMeans->AddPointError(adc_channel, avg_residual, 0, rms_residual);
+			gResidualMeans->SetTitle("Mean vs ADC Chan; ADC Chan; #mu_{residual}");
+			gResidualMeans->SetMarkerStyle(8);
+			gResidualMeans->Draw("AP");
 		}
-		gRange[chan]->SetTitle(Form("Soft. Chan %d vs Time; tStmp [ms]; ch%d_data",chan, chan));
-		gRange[chan]->Draw("AP");
-
-		// Fit with the exact range we care about
-		TF1* fit = new TF1(Form("fit_soft_chan%d", chan), "pol1", extrema.min, extrema.max);
-		gRange[chan]->Fit(fit, "R");
-
-		// Draw slope vs Chan
-		cSlope->cd();
-		gSlope->AddPointError(chan, fit->GetParameter(1), 0, fit->GetParError(1));
-		gSlope->SetTitle("Slope Vs Chan; Chan; Slope");
-		gSlope->SetMarkerStyle(8);
-		gSlope->Draw("AP");
-
-		// Draw Intercept vs Chan
-		cIntercept->cd();
-		gIntercept->AddPointError(chan, fit->GetParameter(0), 0, fit->GetParError(0));
-		gIntercept->SetTitle("Intercept Vs Chan; Chan; Intercept");
-		gIntercept->SetMarkerStyle(8);
-		gIntercept->Draw("AP");
-
-
-		// Compute Residual
-		cResidual->cd(chan+1);
-		double average_residual = 0;
-		size_t residual_counter = 0;
-		for( size_t index = 0; index < voltage.size(); index++ ) {
-			double time   = timestamps[index];
-			double actual = voltage[index];
-			double eval   = fit->Eval(time);
-			double residual = eval - actual;
-			average_residual += residual;
-			residual_counter++;
-			// std::cout << residual_counter << "\t" << residual << "\n";
-			gResidual[chan]->AddPoint(time,residual); 
-		}
-		average_residual /= residual_counter;
-		double rms_residual = GetRMS(voltage, average_residual);
-
-		gResidual[chan]->SetTitle(Form("Residual Vs Chan%d;Chan %d; Residual", chan, chan));
-		gResidual[chan]->Draw("AP");	
-		std::cout << "Avg Residual: " << average_residual << "\n";
-		std::cout << "RMS: " << rms_residual << "\n";
-
-		cResidualMeans->cd();
-		gResidualMeans->AddPointError(chan, average_residual, 0, rms_residual);
-		gResidualMeans->SetTitle("Mean vs Chan; Chan; #mu_{residual}");
-		gResidualMeans->SetMarkerStyle(8);
-		gResidualMeans->Draw("AP");
 	}
 
 	cResidualMeans->Write("cResidual");
