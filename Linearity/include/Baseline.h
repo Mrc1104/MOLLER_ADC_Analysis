8bc3ee2054b94b561be74d61364e393eef7ab3a2
Ryan Conaway <mc321015@ohio.edu>
2025-12-03 10:22:24 -0500

    Adds Proper Baseline csv parsing

--- Linearity/include/Baseline.h
+++ Linearity/include/Baseline.h
@@ -13,13 +13,13 @@ private:
 		double   rms;
 	};
 	friend std::ifstream& operator>>(std::ifstream& in ,       Baseline::baseline_data_structure &data);
-	friend std::ofstream& operator<<(std::ofstream& out, const Baseline::baseline_data_structure &data);
+	friend std::ostream& operator<<(std::ostream& out, const Baseline::baseline_data_structure &data);
 	std::vector<baseline_data_structure> baselines;
 public:
 	// \brief Get baseline from a csv file (see baseline_script.C)
 	Baseline(std::ifstream &fcsv);
-
-
+	double GetMean(unsigned channel);
+	double GetRMS(unsigned channel);
 };
 
 #endif
--- Linearity/src/Baseline.cpp
+++ Linearity/src/Baseline.cpp
@@ -1,12 +1,15 @@
 #include "Baseline.h"
 
+#include <iostream>
+#include <string>
+
 std::ifstream& operator>>(std::ifstream& in, Baseline::baseline_data_structure &data)
 {
-	if(in.peek() != '#')
-		in >> data.adc_channel >> data.mean >> data.rms;
+	char delim[2];
+	in >> data.adc_channel >> delim[0] >> data.mean >> delim[1] >> data.rms;
 	return in;
 }
-std::ofstream& operator<<(std::ofstream& out, const Baseline::baseline_data_structure &data)
+std::ostream& operator<<(std::ostream& out, const Baseline::baseline_data_structure &data)
 {
 	out << "\tadc_channel = " << data.adc_channel << "\n";
 	out << "\tmean = "        << data.mean        << "\n";
@@ -17,8 +20,30 @@ std::ofstream& operator<<(std::ofstream& out, const Baseline::baseline_data_stru
 
 Baseline::Baseline(std::ifstream &fcsv)
 {
+	// Remove First line comment '#'
+	std::string comment;
+	getline(fcsv, comment);
+
 	baseline_data_structure tmp;
 	while(fcsv >> tmp) {
-		baselines.push_back( std::move(tmp) );
+		baselines.emplace_back( std::move(tmp) );
 	}
+}
+
+double Baseline::GetMean(unsigned channel)
+{
+	for(const auto& baseline : baselines) {
+		if(baseline.adc_channel == channel)
+			return baseline.mean;
+	}
+	throw std::runtime_error("Selected Channel was not found!");
+}
+
+double Baseline::GetRMS(unsigned channel)
+{
+	for(const auto& baseline : baselines) {
+		if(baseline.adc_channel == channel)
+			return baseline.rms;
+	}
+	throw std::runtime_error("Selected Channel was not found!");
 }
